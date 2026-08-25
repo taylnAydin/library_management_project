38e49d7a4f43d9c6ea9eadb15fe1df27e913246c
HPRKST46 <HPRKST46@HPRKST46>
2026-08-25 13:59:09 +0300

    RentelCreateDto was added

---
+++ LibraryManagement.Core/DTOs/RentalCreateDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryManagement.Core.DTOs
+{
+    public class RentalCreateDto
+    {
+        public int BookId { get; set; }
+        public int UserId { get; set; }
+        public DateOnly StartDate { get; set; }
+        public DateOnly DueDate { get; set; }
+    }
+}
