bdc61ab404ec620c178f65f2d200e80ab7acd58f
HPRKST46 <HPRKST46@HPRKST46>
2026-08-25 16:27:11 +0300

    complete core layer

---
+++ LibraryManagement.Core/Interfaces/IRentalRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using LibraryManagement.Core.Entities.Concrete;
+
+namespace LibraryManagement.Core.Interfaces
+{
+    public interface IRentalRepository : IGenericRepository<RentedLog>
+    {
+
+        Task<IReadOnlyList<RentedLog>> GetAllWithDetailsAsync();
+
+
+        Task<RentedLog?> GetByIdWithDetailsAsync(int id);
+    }
+}
---
+++ LibraryManagement.Core/Interfaces/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using LibraryManagement.Core.Entities.Concrete;
+
+namespace LibraryManagement.Core.Interfaces
+{
+    public interface IUserRepository : IGenericRepository<User>
+    {
+   
+        Task<User?> GetByEmailAsync(string email);
+
+     
+        Task<User?> GetByIdentificationNumberAsync(string identificationNumber);
+
+      
+        Task<User?> GetUserWithRentalsAsync(int userId);
+    }
+}
