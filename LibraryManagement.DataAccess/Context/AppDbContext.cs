f1de2ef245986d86ef3717bf904f411f20e47e69
Taylan Kemal AYDIN <taylanaydin22@hotmail.com>
2026-08-30 20:11:18 +0300

    add migration

--- LibraryManagement.DataAccess/Context/AppDbContext.cs
+++ LibraryManagement.DataAccess/Context/AppDbContext.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using LibraryManagement.Core.Entities.Concrete;
 
 namespace LibraryManagement.DataAccess.Context
@@ -12,68 +9,200 @@ namespace LibraryManagement.DataAccess.Context
         public DbSet<User> Users { get; set; }
         public DbSet<RentedLog> RentedLogs { get; set; }
 
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { 
-        
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-            // niye degiskene atamadik
-            modelBuilder.Entity<User>(entity => {
-                entity.ToTable("users");
-                entity.HasKey(u => u.Id); // İd dogrudan veremem patlar
-                entity.Property(u => u.Id).HasColumnName("id");
-                entity.Property(u => u.Name).HasColumnName("name");
-                entity.Property(u => u.Surname).HasColumnName("surname");
-                entity.Property(u=>u.Email).HasColumnName("email");
-                entity.Property(u=> u.Password).HasColumnName("password");
-                entity.Property(u => u.Phone).HasColumnName("phone");
-                entity.Property(u => u.BirthdayDate).HasColumnName("birthday_date");
-                entity.Property(u => u.Role).HasColumnName("role").HasConversion<string>();
-                entity.Property(u => u.Gender).HasColumnName("gender").HasConversion<string>(); //hasconversion ne
-                entity.Property(u => u.Country).HasColumnName("country");
-                entity.Property(u => u.IdentityCardNo).HasColumnName("identity_card_no");
-                entity.Property(u => u.IsActive).HasColumnName("is_active");
-                entity.Property(u => u.IsDeleted).HasColumnName("is_deleted");
 
+
+            // USERS
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.ToTable("users", table =>
+                {
+                    table.HasCheckConstraint(
+                        "users_gender_check",
+                        "gender IN ('FEMALE', 'MALE')");
+
+                    table.HasCheckConstraint(
+                        "users_role_check",
+                        "role IN ('LIBRARIAN', 'MEMBER')");
+                });
+
+                entity.HasKey(u => u.Id);
+
+                entity.Property(u => u.Id)
+                    .HasColumnName("id")
+                    .UseIdentityAlwaysColumn();
+
+                entity.Property(u => u.Name)
+                    .HasColumnName("name")
+                    .HasMaxLength(50);
+
+                entity.Property(u => u.Surname)
+                    .HasColumnName("surname")
+                    .HasMaxLength(50);
+
+                entity.Property(u => u.Email)
+                    .HasColumnName("email")
+                    .HasMaxLength(150);
+
+                entity.Property(u => u.Password)
+                    .HasColumnName("password")
+                    .HasMaxLength(255);
+
+                entity.Property(u => u.Phone)
+                    .HasColumnName("phone")
+                    .HasMaxLength(20);
+
+                entity.Property(u => u.BirthdayDate)
+                    .HasColumnName("birthday_date");
+
+                entity.Property(u => u.Role)
+                    .HasColumnName("role")
+                    .HasMaxLength(20)
+                    .HasConversion<string>();
+
+                entity.Property(u => u.Gender)
+                    .HasColumnName("gender")
+                    .HasMaxLength(10)
+                    .HasConversion<string>();
+
+                entity.Property(u => u.Country)
+                    .HasColumnName("country")
+                    .HasMaxLength(50);
+
+                entity.Property(u => u.IdentityCardNo)
+                    .HasColumnName("identity_card_no")
+                    .HasMaxLength(20);
+
+                entity.Property(u => u.IsActive)
+                    .HasColumnName("is_active")
+                    .HasDefaultValue(true);
+
+                entity.Property(u => u.IsDeleted)
+                    .HasColumnName("is_deleted")
+                    .HasDefaultValue(false);
+
+                entity.HasIndex(u => u.Email)
+                    .IsUnique();
+
+                entity.HasIndex(u => u.IdentityCardNo)
+                    .IsUnique();
             });
 
+
+            // BOOKS
             modelBuilder.Entity<Book>(entity =>
             {
-                entity.ToTable("books");
+                entity.ToTable("books", table =>
+                {
+                    table.HasCheckConstraint(
+                        "books_pages_check",
+                        "pages > 0");
+
+                    table.HasCheckConstraint(
+                        "books_stock_check",
+                        "stock >= 0");
+                });
+
                 entity.HasKey(b => b.Id);
-                entity.Property(b => b.Id).HasColumnName("id");
-                entity.Property(b => b.Title).HasColumnName("title");
-                entity.Property(b => b.Writer).HasColumnName("writer");
-                entity.Property(b=> b.Category).HasColumnName("category");
-                entity.Property(b => b.Stock).HasColumnName("stock");
-                entity.Property(b => b.PublishDate).HasColumnName("publish_date");
-                entity.Property(b => b.Publisher).HasColumnName("publisher");
-                entity.Property(b => b.AddedDate).HasColumnName("added_date");
-                entity.Property(b => b.Pages).HasColumnName("pages");
-                entity.Property(b => b.IsDeleted).HasColumnName("is_deleted");
+
+                entity.Property(b => b.Id)
+                    .HasColumnName("id")
+                    .UseIdentityAlwaysColumn();
+
+                entity.Property(b => b.Title)
+                    .HasColumnName("title")
+                    .HasMaxLength(150);
+
+                entity.Property(b => b.Writer)
+                    .HasColumnName("writer")
+                    .HasMaxLength(100);
+
+                entity.Property(b => b.Category)
+                    .HasColumnName("category")
+                    .HasMaxLength(50);
+
+                entity.Property(b => b.Stock)
+                    .HasColumnName("stock")
+                    .HasDefaultValue(0);
+
+                entity.Property(b => b.PublishDate)
+                    .HasColumnName("publish_date");
+
+                entity.Property(b => b.Publisher)
+                    .HasColumnName("publisher")
+                    .HasMaxLength(100);
+
+                entity.Property(b => b.AddedDate)
+                    .HasColumnName("added_date")
+                    .HasDefaultValueSql("CURRENT_DATE");
+
+                entity.Property(b => b.Pages)
+                    .HasColumnName("pages");
+
+                entity.Property(b => b.IsDeleted)
+                    .HasColumnName("is_deleted")
+                    .HasDefaultValue(false);
             });
 
+
+            // RENTED LOGS
             modelBuilder.Entity<RentedLog>(entity =>
             {
-                entity.ToTable("rented_logs");
-                entity.HasKey(r=>r.Id);
-                entity.Property(r => r.Id).HasColumnName("id");
-                entity.Property(r=>r.BookId).HasColumnName("book_id");
-                entity.Property(r=>r.UserId).HasColumnName("user_id");
-                entity.Property(r => r.StartDate).HasColumnName("start_date");
-                entity.Property(r => r.DueDate).HasColumnName("due_date");
-                entity.Property(r => r.ReturnDate).HasColumnName("return_date");
-                entity.Property(r => r.Status).HasColumnName("status").HasConversion<string>();
-                entity.Property(r => r.IsDeleted).HasColumnName("is_deleted");
+                entity.ToTable("rented_logs", table =>
+                {
+                    table.HasCheckConstraint(
+                        "rented_logs_status_check",
+                        "status IN ('RENTED', 'RETURNED')");
+                });
+
+                entity.HasKey(r => r.Id);
+
+                entity.Property(r => r.Id)
+                    .HasColumnName("id")
+                    .UseIdentityAlwaysColumn();
+
+                entity.Property(r => r.BookId)
+                    .HasColumnName("book_id");
+
+                entity.Property(r => r.UserId)
+                    .HasColumnName("user_id");
+
+                entity.Property(r => r.StartDate)
+                    .HasColumnName("start_date")
+                    .HasDefaultValueSql("CURRENT_DATE");
+
+                entity.Property(r => r.DueDate)
+                    .HasColumnName("due_date");
+
+                entity.Property(r => r.ReturnDate)
+                    .HasColumnName("return_date");
+
+                entity.Property(r => r.Status)
+                    .HasColumnName("status")
+                    .HasMaxLength(20)
+                    .HasConversion<string>()
+                    .HasDefaultValueSql("'RENTED'");
+
+                entity.Property(r => r.IsDeleted)
+                    .HasColumnName("is_deleted")
+                    .HasDefaultValue(false);
+
+                entity.HasOne(r => r.Book)
+                    .WithMany()
+                    .HasForeignKey(r => r.BookId)
+                    .OnDelete(DeleteBehavior.NoAction);
+
+                entity.HasOne(r => r.User)
+                    .WithMany(u => u.RentedLogs)
+                    .HasForeignKey(r => r.UserId)
+                    .OnDelete(DeleteBehavior.NoAction);
             });
-
-
         }
-
-
-
-
     }
 }
---
+++ LibraryManagement.DataAccess/Migrations/20260830170656_InitialCreate.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace LibraryManagement.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "books",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityAlwaysColumn),
+                    title = table.Column<string>(type: "character varying(150)", maxLength: 150, nullable: false),
+                    writer = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    category = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    stock = table.Column<int>(type: "integer", nullable: false, defaultValue: 0),
+                    publish_date = table.Column<DateOnly>(type: "date", nullable: false),
+                    publisher = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    added_date = table.Column<DateOnly>(type: "date", nullable: false, defaultValueSql: "CURRENT_DATE"),
+                    pages = table.Column<int>(type: "integer", nullable: false),
+                    is_deleted = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_books", x => x.id);
+                    table.CheckConstraint("books_pages_check", "pages > 0");
+                    table.CheckConstraint("books_stock_check", "stock >= 0");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityAlwaysColumn),
+                    name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    surname = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    email = table.Column<string>(type: "character varying(150)", maxLength: 150, nullable: false),
+                    password = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    phone = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    birthday_date = table.Column<DateOnly>(type: "date", nullable: false),
+                    role = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    gender = table.Column<string>(type: "character varying(10)", maxLength: 10, nullable: false),
+                    country = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    identity_card_no = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    is_active = table.Column<bool>(type: "boolean", nullable: false, defaultValue: true),
+                    is_deleted = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.id);
+                    table.CheckConstraint("users_gender_check", "gender IN ('FEMALE', 'MALE')");
+                    table.CheckConstraint("users_role_check", "role IN ('LIBRARIAN', 'MEMBER')");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "rented_logs",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityAlwaysColumn),
+                    user_id = table.Column<int>(type: "integer", nullable: false),
+                    book_id = table.Column<int>(type: "integer", nullable: false),
+                    start_date = table.Column<DateOnly>(type: "date", nullable: false, defaultValueSql: "CURRENT_DATE"),
+                    due_date = table.Column<DateOnly>(type: "date", nullable: false),
+                    return_date = table.Column<DateOnly>(type: "date", nullable: true),
+                    status = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false, defaultValueSql: "'RENTED'"),
+                    is_deleted = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_rented_logs", x => x.id);
+                    table.CheckConstraint("rented_logs_status_check", "status IN ('RENTED', 'RETURNED')");
+                    table.ForeignKey(
+                        name: "FK_rented_logs_books_book_id",
+                        column: x => x.book_id,
+                        principalTable: "books",
+                        principalColumn: "id");
+                    table.ForeignKey(
+                        name: "FK_rented_logs_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_rented_logs_book_id",
+                table: "rented_logs",
+                column: "book_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_rented_logs_user_id",
+                table: "rented_logs",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_email",
+                table: "users",
+                column: "email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_identity_card_no",
+                table: "users",
+                column: "identity_card_no",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "rented_logs");
+
+            migrationBuilder.DropTable(
+                name: "books");
+
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
